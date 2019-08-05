1a81638590759e0c155ed401535ccf51d830df1d
fun <lljxx1@gmail.com>
2019-08-06 00:41:23 +0800

    switch

--- android/app/src/main/kotlin/com/fun/android_test_kit/MainActivity.kt
+++ android/app/src/main/kotlin/com/fun/android_test_kit/MainActivity.kt
@@ -5,25 +5,11 @@ import android.os.Bundle
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 import android.R.attr.start
-import android.util.Log
-import org.liquidplayer.service.MicroService
-import java.net.URI
 
 
 class MainActivity: FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this);
-      Log.d("MicroService", "try start");
-    val service = MicroService(
-            this,
-            URI("http://my.server.com/path/to/code.js"),
-            object : MicroService.ServiceStartListener{
-              override fun onStart(service: MicroService) {
-                Log.d("MicroService", "started");
-              }
-            }
-    )
-    //service.start()
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,238 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+import 'package:flutter_liquidcore/liquidcore.dart';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+void main() {
+  //enableLiquidCoreLogging = true;
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Random _rng;
+  MicroService _microService;
+  JSContext _jsContext;
+
+  String _jsContextResponse = '<empty>';
+  String _microServiceResponse = '<empty>';
+  int _microServiceWorld = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('FlutterLiquidcore App'),
+        ),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              RaisedButton(
+                child: const Text('MicroService'),
+                onPressed: initMicroService,
+              ),
+              Center(
+                child: Text('MicroService response: $_microServiceResponse\n'),
+              ),
+              RaisedButton(
+                child: const Text('Execute JSContext'),
+                onPressed: _initializeJsContext,
+              ),
+              Center(
+                child: Text('JSContext response: $_jsContextResponse\n'),
+              )
+            ]),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+  void dispose() {
+    if (_microService != null) {
+      // Exit and free up the resources.
+      // _microService.exitProcess(0); // This API call might not always be available.
+      _microService.emit('exit');
+    }
+    if (_jsContext != null) {
+      // Free up the context resources.
+      _jsContext.cleanUp();
+    }
+    super.dispose();
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  void _initializeJsContext() async {
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      if (_jsContext == null) {
+        _jsContext = new JSContext();
+        const code = """
+        // Attached as a property of the current global context scope.
+        var obj = {
+          number: 1,
+          string: 'string',
+          date: new Date(),
+          array: [1, 'string', null, undefined],
+          func: function () {}
+        };
+        var a = 10;
+        // Is a variable, and not attached as a property of the context.
+        let objLet = { number: 1, yayForLet: true };
+        """;
+        await _jsContext.evaluateScript(code);
+        try {
+          // Evaluate an invalid javascript call.
+          await _jsContext.evaluateScript('invalid.call()');
+        } catch(e) {
+          print(e);
+        }
+        try {
+          // Catch an exception.
+          await _jsContext.evaluateScript('throw new Error("My exception message")');
+        } catch(e) {
+          print(e);
+        }
+        // This will return a promise object, but you won't be able to manipulate it from Dart.
+        var promise = await _jsContext.evaluateScript('''
+            var response;
+            (async () => {
+              response = await Promise.reject();
+            })();
+            ''');
+        var obj = await _jsContext.property("obj");
+        var aValue = await _jsContext.property("a");
+        var objLet = await _jsContext.evaluateScript("objLet");
 
-  void _incrementCounter() {
+        // Add factorial function.
+        await _jsContext.setProperty("factorial", (double x) {
+          print("factorial($x)");
+          int factorial = 1;
+          for (; x > 1; x--) {
+            factorial *= x.toInt();
+          }
+          return factorial;
+        });
+        // Return a declared function (currently only works with dart functions).
+        var factorialFn = await _jsContext.property("factorial");
+
+        await _jsContext.setProperty("factorialThen",
+                (double factorial) async {
+              var f = await _jsContext.property("f");
+              print("factorialThen($f) = $factorial");
+              _setJsContextResponse(
+                  "Factorial of ${f.toInt()} = ${factorial.toInt()} !");
+
+              return factorial;
+            });
+
+        print("******************************");
+        print("obj = $obj");
+        print("a = $aValue");
+        print("promise = $promise");
+        print("objLet = $objLet");
+        print("factorialFn = ${factorialFn.runtimeType.toString()}");
+        print("******************************");
+      }
+
+      if (_rng == null) {
+        _rng = new Random();
+      }
+      // Generate a random number.
+      var factorialNumber = _rng.nextInt(10);
+      await _jsContext.setProperty("f", factorialNumber);
+      await _jsContext.evaluateScript("factorial(f).then(factorialThen);");
+
+      //returnVal = await context.evaluateScript("( function(){ return factorial($factorialNumber).then(factorialThen) })() ");
+    } on PlatformException {
+      _setJsContextResponse('Failed to get factorial from Javascript. $e');
+    }
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  void initMicroService() async {
+    if (_microService == null) {
+      String uri;
+
+      // Android doesn't allow dashes in the res/raw directory.
+      //uri = "android.resource://io.jojodev.flutter.liquidcoreexample/raw/liquidcore_sample";
+//      uri = "@flutter_assets/Resources/liquidcore_sample.js";
+      uri = "http://192.168.1.8:8080/index.js";
+
+      _microService = new MicroService(uri);
+      await _microService.addEventListener('ready',
+              (service, event, eventPayload) {
+            // The service is ready.
+            if (!mounted) {
+              return;
+            }
+
+            print('ready '+uri);
+            //_emit();
+          });
+      await _microService.addEventListener('pong',
+              (service, event, eventPayload) {
+            if (!mounted) {
+              return;
+            }
+
+            _setMicroServiceResponse(eventPayload['message']);
+          });
+      await _microService.addEventListener('object',
+              (service, event, eventPayload) {
+            if (!mounted) {
+              return;
+            }
+
+            print("received obj: $eventPayload | type: ${eventPayload.runtimeType}");
+          });
+
+      // Start the service.
+      await _microService.start();
+    }
+
+    if (_microService.isStarted) {
+      _emit();
+    }
+  }
+
+  void _emit() async {
+    // Send the name over to the MicroService.
+    await _microService.emit('ping', 'World ${++_microServiceWorld}');
+  }
+
+  void _setMicroServiceResponse(message) {
+    if (!mounted) {
+      print("microService: widget not mounted");
+      return;
+    }
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _microServiceResponse = message;
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  void _setJsContextResponse(value) {
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) {
+      print("jsContext: widget not mounted");
+      return;
+    }
+
+    setState(() {
+      _jsContextResponse = value;
+    });
   }
 }
